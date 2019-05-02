257536587926c4ea51f630fe9a5c4a52955d3594
Nickolas Gamblin <nickgamblin@gmail.com>
2019-05-01 22:37:34 -0500

    needs moment

---
+++ assets/logic.js
@@ -0,0 +1,76 @@
+function test(){
+console.log("hi")
+}
+test();
+
+
+var config = {
+    apiKey: "AIzaSyA0bQpRvj22zD_ziftaEwsejcxOQFoG7W4",
+    authDomain: "nickdb-6dea5.firebaseapp.com",
+    databaseURL: "https://nickdb-6dea5.firebaseio.com",
+    projectId: "nickdb-6dea5",
+    storageBucket: "nickdb-6dea5.appspot.com",
+    messagingSenderId: "161121778808"
+  };
+
+  firebase.initializeApp(config);
+
+  var database = firebase.database();
+
+//  Button for adding trains
+$("#addTrain").on("click", function(event) {
+  event.preventDefault();
+
+  // store user input
+  var routeName = $("#route").val().trim();
+  var endDest = $("#endDestination").val().trim();
+  var firstRun = $("#firstRun").val().trim();
+  var frequency = $("#frequency").val().trim();
+
+  var newTrain = {
+    routeName: routeName,
+    endDest: endDest,
+    firstRun: firstRun,
+    frequency: frequency
+  };
+
+  //  train data to the firebase
+  database.ref().push(newTrain);
+
+  // console.log(newTrain.routeName);
+  // console.log(newTrain.endDest);
+  // console.log(newTrain.firstRun);
+  // console.log(newTrain.frequency);
+
+  // Clears form
+  $("#route").val("");
+  $("#endDestination").val("");
+  $("#firstRun").val("");
+  $("#frequency").val("");
+});
+
+// add train to firebase and then row to train table
+database.ref().on("child_added", function(childSnapshot) {
+  console.log(childSnapshot.val());
+
+  var routeName = childSnapshot.val().routeName;
+  var endDestination = childSnapshot.val().endDestination;
+  var firstRun = childSnapshot.val().firstRun;
+  var frequency = childSnapshot.val().frequency;
+
+// console.log(routeName);
+// console.log(endDestination);
+// console.log(firstRun);
+// console.log(frequency);
+
+  var newRow = $("<tr>").append(
+    $("<td>").text(routeName),
+    $("<td>").text(endDestination),
+    $("<td>").text(firstRun),
+    $("<td>").text(frequency),
+ 
+  );
+
+  // Append the new row to the table
+  $("#trainSchedule > tbody").append(newRow);
+});
